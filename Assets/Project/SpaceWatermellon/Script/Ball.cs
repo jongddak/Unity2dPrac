8fd67b38139c397200053cb49077b022422625a1
jongddak <hfsmsj316@gmail.com>
2024-09-14 21:27:24 +0900

    주석수정
    주석수정

--- Assets/Project/SpaceWatermellon/Script/Ball.cs
+++ Assets/Project/SpaceWatermellon/Script/Ball.cs
@@ -10,20 +10,19 @@ public class Ball : MonoBehaviour
     {
         if (collision.gameObject.tag != "sixth")
         {
-            // 태그가 같은지 확인
+            
             if (collision.gameObject.CompareTag(gameObject.tag))
             {
 
                 if (gameObject.GetInstanceID() < collision.gameObject.GetInstanceID())
                 {
-                    // 충돌한 위치에서 새로운 오브젝트 생성
+                    
                     Vector3 collisionPosition = collision.transform.position;
 
-                    // 충돌한 두 오브젝트 제거
+                   
                     Destroy(collision.gameObject);
                     Destroy(gameObject);
 
-                    // 새로운 오브젝트 생성
                     Instantiate(prepabs[0], collisionPosition, Quaternion.identity);
                 }
             }
